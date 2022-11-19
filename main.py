d56a63d6706708d14e3f85792dc80c7bdc691312
wz-fu <69373512+wz-fu@users.noreply.github.com>
2022-11-19 11:06:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_words():
   words = requests.get("https://api.iculture.cc/api/yiyan")
   if words.status_code != 200:
     return get_words()
-  return words.json()['data']['text']
+  return words.text
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
